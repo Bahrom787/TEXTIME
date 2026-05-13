616e65b3fdace95af11deec32ac0c8e3e2aebd84
Bakhrom <rahmatullaevbahrom616@gmail.com>
2026-05-13 22:15:38 +0500

    Add base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/TEXTIME/',
   plugins: [react()],
   css: {
     devSourcemap: true
